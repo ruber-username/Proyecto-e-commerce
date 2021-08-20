4e94792ab3c408bac4ced2d9f945156bbb429c2a
ruber-username <88205179+ruber-username@users.noreply.github.com>
2021-08-20 14:10:01 -0300

    Primer entregable

--- js/login.js
+++ js/login.js
@@ -1,6 +1,30 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
 
-});
+
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById("botón").addEventListener("click", function(){
+
+            let email = document.getElementById("email");
+            let contraseña= document.getElementById("contraseña");
+            let campo_completo = true;
+
+            if(email.value === ""){
+                email.classList.add("invalid");
+                campo_completo = false;
+            } else{
+                email.classList.remove("invalid");
+            }
+
+            if(contraseña.value === ""){
+                contraseña.classList.add("invalid");
+                campo_completo = false;
+            } else {
+                contraseña.classList.remove("invalid");
+            }
+
+            if(campo_completo){window.location.href = 'inicio.html';}else{alert("Upss! Usuario o contraseña incorrectos :(");}
+
+        
+});})
--- js/products.js
+++ js/products.js
@@ -2,5 +2,52 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
-
+getJSONData(PRODUCTS_URL).then(function(resultObj){
+    if (resultObj.status === "ok"){
+            lista_de_productos(PRODUCTS_URL);
+        }
+    });
 });
+
+
+function lista_de_productos(url){
+
+    document.getElementById("data1").innerHTML= "";
+    fetch(url)
+    .then(respuesta => respuesta.json())
+    .then(datos =>{
+
+        for(let i=0; i<datos.length; i++)
+        {
+            let imagen="";
+
+            if(i===1){
+                imagen='<img src="img/prod1.jpg">';
+            }else{
+                if(i===2){
+                    imagen='<img src="img/prod2.jpg">';
+                }else{
+                    if(i===3){
+                        imagen='<img src="img/prod3.jpg">';
+                    }else{
+                        imagen='<img src="img/prod4.jpg">';
+                    }
+                }
+            }
+
+                        let row="";
+                        row= `
+                            <tr>
+                            <td>`+ datos[i].name +`</td>
+                            <td>`+ datos[i].description +`</td>
+                            <td>`+ datos[i].cost + ` ` + datos[i].currency +`</td>
+                            <td>`+ datos[i].soldCount +`</td>
+                            <td>`+ imagen +`</td>  
+                            </tr>
+                            `;
+                document.getElementById("data1").innerHTML += row;
+        };
+
+    })
+    .catch(error => alert("Error" + error));
+}
